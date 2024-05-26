eb3c746bbcb05ae255146fd55c145c67c47e7deb
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2024-05-26 20:07:02 +0300

    all files from repo

---
+++ src/mongoDB/models/Product.js
@@ -0,0 +1,21 @@
+
+const { Schema, model } = require("mongoose");
+
+const Product_schema = new Schema({
+    category: { type: String, required: true },
+    discount_price: { type: Number },
+    name: { type: String, required: true, unique: true },
+    price: { type: Number, required: true },
+    unit: { type: String },
+    available: { type: Boolean },
+    description: { type: String },
+    favourite: { type: Boolean },
+    img: { type: String },
+    archived: { type: Boolean }
+},
+    {
+        versionKey: false,
+        timestamps: true
+    });
+
+module.exports = model("Product", Product_schema);
